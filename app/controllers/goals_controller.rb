15d409d5220218016bbbe41b9c068d24ceae3ff8
arya mokhber <amokhber94@gmail.com>
2022-05-17 20:29:15 -0400

    added the controller actions

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,15 +1,33 @@
 class GoalsController < ApplicationController
   # I can add a goal
   def new
+    @goal = Goal.new
   end
 
   def create
+    @goal = Goal.new(goal_params)
+    @goal.user = current_user
+    if @goal.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
   end
 
   # I can edit a goal
-  def edit
-  end
 
   def update
+    @goal = Goal.find(params[:id])
+    if @goal.update(goal_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:job_target, :description)
   end
 end
