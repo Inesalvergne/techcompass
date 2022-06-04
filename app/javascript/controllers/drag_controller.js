019a9e3cd5994c8e67d2af0de0806d0ef84416cd
Ines Alvergne <i.alvergne@gmail.com>
2022-06-04 12:14:04 -0400

    drag and drop working on the front-end side

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -1,6 +1,6 @@
-// Visit The Stimulus Handbook for more details 
+// Visit The Stimulus Handbook for more details
 // https://stimulusjs.org/handbook/introduction
-// 
+//
 // This example controller works with specially annotated HTML like:
 //
 // <div data-controller="hello">
@@ -8,11 +8,26 @@
 // </div>
 
 import { Controller } from "stimulus"
+import Sortable from "sortablejs"
 
 export default class extends Controller {
-  static targets = [ "output" ]
+
+  // static targets = ["wishlist", "applied", "interview", "decision", "offer", "rejected"]
 
   connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+    console.log('Hello, Stimulus!')
+    this.sortable = Sortable.create(this.element, {
+      group: "status",
+    })
+
+    //  Sortable.create(this.wishlist, {
+    //   group: 'shared', // set both lists to same group
+    //   animation: 150
+    //  });
+
+    // Sortable.create(this.applied, {
+    //     group: 'shared',
+    //     animation: 150
+    // });
   }
 }
