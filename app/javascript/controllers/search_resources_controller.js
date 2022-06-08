ca31295f7366b8db6fdda1dc80d9c62936a6cd36
Ines Alvergne <i.alvergne@gmail.com>
2022-06-07 20:43:19 -0400

    ajax is implemented on the search bar

--- app/javascript/controllers/search_resources_controller.js
+++ app/javascript/controllers/search_resources_controller.js
@@ -8,4 +8,14 @@ export default class extends Controller {
     console.log(this.inputTarget)
     console.log(this.listTarget)
   }
+
+  update() {
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    console.log(url)
+    fetch(url, { headers: { "Accept": "text/plain" } })
+    .then(response => response.text())
+    .then((data) => {
+      this.listTarget.outerHTML = data
+    })
+  }
 }
